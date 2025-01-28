e1da2b99f613c036954313039b78eff3a640bc87
Aditya Byju <byjuaditya@gmail.com>
2025-01-27 20:05:59 -0600

    Fix loading error

--- src/components/global/Loading.jsx
+++ src/components/global/Loading.jsx
@@ -1,7 +1,13 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
+import dynamic from "next/dynamic";
+
+// Dynamically import the Player component with SSR disabled
+const Player = dynamic(
+  () => import("@lottiefiles/react-lottie-player").then((mod) => mod.Player),
+  { ssr: false }
+);
 
 const Loading = () => {
   const [animationData, setAnimationData] = useState(null);
